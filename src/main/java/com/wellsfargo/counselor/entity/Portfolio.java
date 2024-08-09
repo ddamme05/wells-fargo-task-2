30159b210dd99e944b849a533f37e3be44c450d7
ddamme05 <ddamme05@gmail.com>
2024-08-09 19:06:33 -0400

    Defined Portfolio class.

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,54 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+
+import java.util.Date;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "clientId", nullable = false, unique = true)
+    private Client client;
+
+    @Column(nullable = false)
+    private Date creationDate;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(Client client, Date creationDate) {
+        this.client = client;
+        this.creationDate = creationDate;
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
